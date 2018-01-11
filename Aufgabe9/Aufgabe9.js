3b4b8d088b51823fca83da5a1be2d47549e1ca13
lisah <lisah@DESKTOP-55DFEF1.speedport.ip>
2018-01-11 21:56:48 +0100

    Hochladen der Aufgabe 9 und Verlinkung im Steckbrief

--- Aufgabe9/Aufgabe9.js
+++ Aufgabe9/Aufgabe9.js
@@ -7,16 +7,61 @@
     nicht kopiert und auch nicht diktiert. */
 var aufgabe9;
 (function (aufgabe9) {
+    window.addEventListener("load", init);
+    let theChosenLetter;
+    document.addEventListener("keydown", keyboardSelection);
+    // Characters 
     function init() {
-        let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "X", "Y", "Z"];
-        let lettercount = letters.length;
+        let letter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "X", "Y", "Z"];
+        let lettercount = letter.length;
+        for (let i = 0; i < letter.length; i++) {
+            drawBoxes(letter[i]);
+        }
+        drawPaper();
     }
-    function drawBoxes() {
+    // Buchstabenboxen zeichnen
+    function drawBoxes(_letter) {
         let div = document.createElement("div");
-        div.style.width = "70px";
-        div.style.height = "70px";
+        div.style.width = "42px";
+        div.style.height = "42px";
         div.style.font = "Arial Bold";
-        div.style.fontSize = "18px";
+        div.style.fontSize = "38px";
+        div.style.backgroundColor = "yellow";
+        div.innerText = _letter;
+        div.id = _letter;
+        div.className = "Buchstaben";
+        document.body.appendChild(div);
+    }
+    // Papier wird gezeichnet
+    function drawPaper() {
+        let paper = document.createElement("div");
+        paper.style.width = "700px";
+        paper.style.height = "950px";
+        paper.style.margin = "5%";
+        paper.style.marginTop = "1%";
+        paper.style.backgroundColor = "black";
+        paper.style.position = "absolute";
+        paper.style.top = "1%";
+        document.body.appendChild(paper);
+        // Was fehlt: Buchstaben in den Brief einf�gen, Buchstaben ausw�hlen & die Fehlermeldung beheben
+    }
+    //Set letters to pap    
+    function insertLetters(_event) {
+        let paperLetterBox = document.createElement("div");
+        paperLetterBox.innerText = "_letter";
+        paperLetterBox.style.backgroundColor = "yellow";
+        paperLetterBox.style.height = "42px";
+        paperLetterBox.style.width = "42px";
+        paperLetterBox.style.fontSize = "38";
+        paperLetterBox.style.right = _event.pageX + "px";
+        paperLetterBox.style.bottom = _event.pageY + "px";
+        document.body.appendChild(paperLetterBox);
+        let mouseClick = _event.target;
+    }
+    function keyboardSelection(event) {
+        if (event.key == "a" || event.key == "A") {
+            theChosenLetter = event.key;
+        }
     }
 })(aufgabe9 || (aufgabe9 = {}));
 //# sourceMappingURL=Aufgabe9.js.map
--- Aufgabe9/Aufgabe9.ts
+++ Aufgabe9/Aufgabe9.ts
@@ -7,37 +7,100 @@
     nicht kopiert und auch nicht diktiert. */
 
 namespace aufgabe9 {
+
+    window.addEventListener("load", init);
+    let theChosenLetter: string;
+
+    document.addEventListener("keydown", keyboardSelection());
+
+    // Characters 
     function init(): void {
         let letter: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "X", "Y", "Z"];
         let lettercount: number = letter.length;
 
         for (let i: number = 0; i < letter.length; i++) {
-            drawBoxes(_letter[i]);
+            drawBoxes(letter[i]);
         }
-        
         drawPaper();
+
     }
-// Buchstabenboxen zeichnen
+
+    // Buchstabenboxen zeichnen
     function drawBoxes(_letter: string): void {
         let div: HTMLDivElement = document.createElement("div");
 
-        div.style.width = "70px";
-        div.style.height = "70px";
+        div.style.width = "42px";
+        div.style.height = "42px";
         div.style.font = "Arial Bold";
-        div.style.fontSize = "18px";
+        div.style.fontSize = "38px";
         div.style.backgroundColor = "yellow";
-        div.innerText = "_letter";
+        div.innerText = _letter;
+        div.id = _letter;
+        div.className = "Buchstaben";
+
+        div.addEventListener = ("click", chooseLetterF);
+        document.body.appendChild(div);
+
     }
 
 
-// Papier wird gezeichnet
-function drawPaper(): void {
-    let paper: HTMLDivElement = document.createElement("div");
+    // Papier wird gezeichnet
+    function drawPaper(): void {
+        let paper: HTMLDivElement = document.createElement("div");
 
-    paper.style.width = "500px";
-    paper.style.height = "700px";
-    paper.style.margin = "5%";
-    paper.style.backgroundColor = "black";
+        paper.style.width = "700px";
+        paper.style.height = "950px";
+        paper.style.margin = "5%";
+        paper.style.marginTop = "1%";
+        paper.style.backgroundColor = "black";
+        paper.style.position = "absolute";
+        paper.style.top = "1%";
 
-// Was fehlt: Buchstaben in den Brief einfügen, Buchstaben auswählen & die Fehlermeldung beheben
-}}
+        document.body.appendChild(paper);
+        // Was fehlt: Buchstaben in den Brief einfügen, Buchstaben auswählen & die Fehlermeldung beheben
+    }
+
+    // choose Letter from List 
+    function chooseLetterF(_event: MouseEvent) {
+
+        let mouseclick: HTMLDivElement = <HTMLDivElement>_event.target;
+
+        mouseclick.style.backgroundColor = "green";
+
+        theChosenLetter = mouseclick.id;
+
+
+        let divs: NodeListOf<HTMLDivElement> = <NodeListOf<HTMLDivElement>>document.getElementsByClassName("Buchstaben");
+
+        for (let i: number = 0; i < divs.length; i++) {
+            if (theChosenLetter != divs[i].id) {
+                divs[i].style.color = "pink";
+            }
+        }
+    }
+    //Set letters to paper    
+    function insertLetters(_event: MouseEvent): void {
+
+        let paperLetterBox: HTMLDivElement = document.createElement("div");
+
+        paperLetterBox.innerText = "_letter";
+        paperLetterBox.style.backgroundColor = "yellow";
+        paperLetterBox.style.height = "42px";
+        paperLetterBox.style.width = "42px";
+        paperLetterBox.style.fontSize = "38";
+        paperLetterBox.style.right = _event.pageX + "px";
+        paperLetterBox.style.bottom = _event.pageY + "px";
+
+        document.body.appendChild(paperLetterBox);
+
+        let mouseClick: HTMLDivElement = <HTMLDivElement>_event.target;
+    }
+
+    function keyboardSelection(event: KeyboardEvent): void {
+        if (event.key == "a" || event.key == "A") {
+            theChosenLetter = event.key;
+        }
+    }
+
+
+}
